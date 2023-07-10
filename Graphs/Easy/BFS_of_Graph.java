302255a99423de4be647607391891a497686e35a
Yashank Tiwari <64902661+yashanktiwari@users.noreply.github.com>
2023-07-11 00:22:10 +0530

    Update BFS_of_Graph.java

--- Graphs/Easy/BFS_of_Graph.java
+++ Graphs/Easy/BFS_of_Graph.java
@@ -5,6 +5,9 @@ Use a queue to store the nodes and follow the 5 steps:
 3. mark it visited
 4. add the node in the list
 5. put all the non visited neighbours in the queue.
+
+TC -> O(N)
+SC -> O(1)
 */
 class Solution {
     // Function to return Breadth First Traversal of given graph.
