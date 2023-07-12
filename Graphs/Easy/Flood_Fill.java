857853ed40e08c4b1e036a3b8e9b055c91e2a3a1
Yashank Tiwari <64902661+yashanktiwari@users.noreply.github.com>
2023-07-12 13:03:03 +0530

    Create Flood_Fill.java

---
+++ Graphs/Easy/Flood_Fill.java
@@ -0,0 +1,30 @@
+/*
+Do simple BFS and color all the connected nodes which have the same initial color
+Maintain a visited array to not fall in the infinite loop.
+
+TC -> O(n*m)
+SC -> O(n*m) (visited array) + stack space (if using recursion)
+*/
+class Solution {
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        int checkColor = image[sr][sc];
+        boolean[][] visited = new boolean[image.length][image[0].length];
+        colorFill(image, visited, sr, sc, color, checkColor);
+        return image;
+    }
+
+    public void colorFill(int[][] image, boolean[][] visited, int sr, int sc, int color, int checkColor) {
+
+        if(sr < 0 || sc < 0 || sr >= image.length || sc >= image[0].length || visited[sr][sc] == true || image[sr][sc] != checkColor) return;
+        
+        image[sr][sc] = color;
+        visited[sr][sc] = true;
+
+        int[] r = {-1, 0, 1, 0};
+        int[] c = {0, -1, 0, 1};
+
+        for(int x=0; x<4; x++) {
+            colorFill(image, visited, sr+r[x], sc+c[x], color, checkColor);
+        }
+    }
+}
